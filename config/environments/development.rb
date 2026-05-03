8875d14673946b1be9a6188cdfa7156b01483b1f
Remi <koro6325@gmail.com>
2026-05-03 11:44:32 +0900

    作業中の変更を保存

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  BetterErrors:Middleware.allow_ip!"0.0.0.0/0" 
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'top#index'
 
   resources :welcomes, only: :index
+  resources :users
 end
