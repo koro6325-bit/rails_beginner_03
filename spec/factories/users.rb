1c7a60ec6eccd67adc60baab8f73c37abdc59f42
Remi <koro6325@gmail.com>
2026-05-06 21:42:58 +0900

    作業保存

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,21 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:name) { |n| "user-#{n}" }
+    role { :writer }
+
+    trait :admin do
+      sequence(:name) { |n| "admin_#{n}" }
+      role { :admin }
+    end
+
+    trait :editor do
+      sequence(:name) { |n| "editor_#{n}" }
+      role { :editor }
+    end
+
+    trait :writer do
+      sequence(:name) { |n| "writer_#{n}" }
+      role { :writer }
+    end
   end
 end
